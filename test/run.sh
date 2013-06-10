ee016b4639d4bd7a160aee7bcf63f9e987e928fc
mogeta <muzigen@gmail.com>
2013-06-11 00:47:01 +0900

    change sh2

--- test/run.sh
+++ test/run.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+echo Analyzing library for warnings or type errors
+
+dartanalyzer --package-root packages --fatal-warnings --fatal-type-errors *.dart || \
+  echo "ignore analyzer errors"
+
+dart --enable-type-checks --enable-asserts test/run_all.dart $@
